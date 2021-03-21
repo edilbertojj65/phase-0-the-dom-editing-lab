ccf39b485111ab193576d79b3a0de571c1ccc07f
edilbertojj65 <edilbertojj65@gmail.com>
2021-03-21 19:06:32 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,5 @@
+//const newbody = document.querySelector('body').innerHTML('<h1>My HTML adventure</h1>');
+//var s = document.createElement('h1');
+//s.type = 'text/javascript';
+//var code = 'My HTML adventure';
+//s.appendChild(document.createTextNode(code))
